fa345206e7b58e7b6797b8d593e16f8e709790af
Mohssin <mabihilal@spartglobal.com>
2019-10-19 15:19:57 +0100

    BMR and BMI working

--- Gym_App/Gym_App/MainWindow.xaml.cs
+++ Gym_App/Gym_App/MainWindow.xaml.cs
@@ -20,12 +20,16 @@ namespace Gym_App
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static string CurrentUserId = null; 
-        
+        public static string CurrentUserId = null;
+
 
         public MainWindow()
         {
             InitializeComponent();
+            // BMI & BMR Text Info Hidden
+            BMI_INFO.Visibility = Visibility.Hidden;
+            BMR_INFO.Visibility = Visibility.Hidden;
+            // Manual Tab Control On main window
             Tab1.IsSelected = true;
             Tab2.IsSelected = false;
         }
@@ -35,7 +39,12 @@ namespace Gym_App
             Tab2.IsSelected = true;
         }
 
-        
+
+
+        private void btn_generateId_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
 
         private void TabController_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -51,7 +60,6 @@ namespace Gym_App
         {
             using (var dbc = new GymAppDBEntities())
             {
-
                 var userToAdd = new User();
                 userToAdd.FirstName = txt_FirstName.Text;
                 userToAdd.LastName = txt_LastName.Text;
@@ -67,16 +75,59 @@ namespace Gym_App
                 healthInfoToAdd.Weight = decimal.Parse(txt_Weight.Text);
 
                 // BMI Calculation Formula
-                //healthInfoToAdd.BMI = (healthInfoToAdd.Weight / healthInfoToAdd.Height / healthInfoToAdd.Height) * 10000;
-                //BMI_Value.Text = healthInfoToAdd.BMI.ToString();
+                double bmi = Math.Round((((double)healthInfoToAdd.Weight / (double)healthInfoToAdd.Height / (double)healthInfoToAdd.Height) * 10000),2);
+                healthInfoToAdd.BMI = (decimal)bmi;
+                BMI_Value.Text = healthInfoToAdd.BMI.ToString();
 
-                //update db
-                dbc.Users.Add(userToAdd);
-                dbc.HealthRecords.Add(healthInfoToAdd);
-                dbc.SaveChanges();
+                // BMR Calculation Formula
 
-                //MessageBox.Show($"User: {userToAdd.FirstName} , successfully added!");
+
+                if (rdo_Female.IsChecked == true)
+                {
+                    double bmr = Math.Round(447.593 + (9.247 * (double)healthInfoToAdd.Weight) + (3.098 * (double)healthInfoToAdd.Height) - (4.330 * (double)healthInfoToAdd.Age), 2);
+                    healthInfoToAdd.BMR = (decimal)bmr;
+                    BMR_Value.Text = healthInfoToAdd.BMR.ToString();
+                }
+                else if (rdo_Male.IsChecked == true)
+                {
+                    double bmr = Math.Round(88.362 + (13.397 * (double)healthInfoToAdd.Weight) + (4.799 * (double)healthInfoToAdd.Height) - (5.677 * (double)healthInfoToAdd.Age), 2);
+                    healthInfoToAdd.BMR = (decimal)bmr;
+                    BMR_Value.Text = healthInfoToAdd.BMR.ToString();
+                }
+                else if (rdo_Female.IsChecked == false && rdo_Male.IsChecked == false)
+                {
+                    MessageBox.Show("Invalid! Please select Male or Female Options");
+                    txt_Age.Clear();
+                    txt_Age.Focus();
+                    txt_Height.Clear();
+                    txt_Weight.Clear();
+                    BMI_Value.Clear();
+                    BMR_Value.Clear();
+
+                    //update db
+                    dbc.Users.Add(userToAdd);
+                    dbc.HealthRecords.Add(healthInfoToAdd);
+                    dbc.SaveChanges();
+                    MessageBox.Show($"User: {userToAdd.FirstName} , successfully added!");
+                }
             }
+        }
+
+        private void lbl_BMI_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BMI_INFO.Visibility = Visibility.Visible;
+        }
+
+        private void lbl_BMI_MouseLeave(object sender, MouseEventArgs e)
+        {
+            BMI_INFO.Visibility = Visibility.Hidden;
+
+        }
+
+        private void rdo_Female_Checked(object sender, RoutedEventArgs e)
+        {
+     
+            
         }
     }
 }
